fd7219567baa023fe00a3a7aad2d3e0e46bdfef4
kush_satyadev <91907695+satyadev2@users.noreply.github.com>
2023-06-08 01:47:08 +0530

    khol k dekh le

--- stringss.cpp
+++ stringss.cpp
@@ -1,7 +1,28 @@
-    #include <bits/stdc++.h>
+// Examples
+// Input
+// Copy
+
+// WUBWUBABCWUB
+
+// Output
+// Copy
+
+// ABC 
+
+// Input
+// Copy
+
+// WUBWEWUBAREWUBWUBTHEWUBCHAMPIONSWUBMYWUBFRIENDWUB
+
+// Output
+// Copy
+
+WE ARE THE CHAMPIONS MY FRIEND     
+#include <bits/stdc++.h>
     using namespace std;
     int main(){
     string s;
     cin>>s;
     cout<<regex_replace(s, regex("WUB"), " ");  // replacing some letters with other one
     }
+
