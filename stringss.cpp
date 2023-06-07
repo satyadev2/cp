e9ce0ca307fab791cb0a63aa209353fda0ed32e5
kush_satyadev <91907695+satyadev2@users.noreply.github.com>
2023-06-08 01:35:07 +0530

    Create stringss.cpp
    khol k dekh le

---
+++ stringss.cpp
@@ -0,0 +1,7 @@
+    #include <bits/stdc++.h>
+    using namespace std;
+    int main(){
+    string s;
+    cin>>s;
+    cout<<regex_replace(s, regex("WUB"), " ");  // replacing some letters with other one
+    }
